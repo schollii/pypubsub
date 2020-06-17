bedb2c00b6299f1a4a6cc8b0a6ac6a82e957f364
Metallicow <metaliobovinus@gmail.com>
2020-06-17 03:14:45 -0500

    Evict Foul Whitespace Demons 2020
    You know the gist `[ \t]+$`

--- src/pubsub/utils/__init__.py
+++ src/pubsub/utils/__init__.py
@@ -1,6 +1,6 @@
 """
-Provides utility functions and classes that are not required for using 
-pubsub but are likely to be very useful. 
+Provides utility functions and classes that are not required for using
+pubsub but are likely to be very useful.
 
 :copyright: Copyright since 2006 by Oliver Schoenborn, all rights reserved.
 :license: BSD, see LICENSE_BSD_Simple.txt for details.
